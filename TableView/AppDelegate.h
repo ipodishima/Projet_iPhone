83a10fd695f01f9c4797f48ffd41f5cd069dd083
Élèves <eleves@Mac-mini-de-Ismin-5.local>
2012-09-21 14:30:01 +0200

    Initial Commit

---
+++ TableView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TableView
+//
+//  Created by Élèves on 21/09/12.
+//  Copyright (c) 2012 EvaRousseau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
